d5d77a599a46dd0ffbcb506263f1347a9fc7057f
yourusername <ogundelee123@gmail.com>
2023-02-02 17:06:31 -0700

    HH

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let button = document.querySelector(".add")
+
+button.addEventListener("click", ()=> {
+    let chapter = document.querySelector("#chapter").value;
+
+    let list = document.querySelector(".list")
+
+    let li = document.createElement("li");
+    let del = document.createElement("button");
+    del.className = "delete";
+    del.textContent = "X"
+    if (chapter != "") {
+        li.appendChild(document.createTextNode(chapter));
+        document.querySelector("#chapter").value = ""
+        li.appendChild(del);
+        
+        list.appendChild(li)
+    } else {
+        alert("You need to add a chapter")
+    }
+    
+    del.addEventListener("click", ()=> {
+        list.removeChild(li)
+    })
+
+})
+
+
+
+
+let date = new Date()
+console.log(date)
+
+let lastUpdate = document.lastModified;
+document.querySelector('.update').innerHTML = `Last Updated ${lastUpdate} <br> ©️ ${date.getFullYear()} Eniola Emmanuel Ogundele`
